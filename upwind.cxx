e9976f4e812a4caab75ebf4ac07abd4684dcb550
Esra Uenal <esuena@pc03.pool.tp1.uni-duesseldorf.de>
2016-01-12 16:00:54 +0100

    Upwind_and_Forward

--- upwind.cxx
+++ upwind.cxx
@@ -10,23 +10,26 @@ void writeToFile(const double* const u, const string s, const double dx,
                  const double xmin, const int N);
 void initialize(double* const u, const double dx, const double xmin,
                 const int N);
+void upwind(double* u1, double* u0, double dt, const double dx, const int N, const double V);
+
+void forward(double* u1, double* u0, double dt, const double dx, const int N, const double V);
 //---------------------------------------
 int main(){
 
-  const double tEnd = ;
-  const double V = ;
+  const double tEnd = 5;
+  const double V = 1;
 
-  const int N  = ;
+  const int N  = 256;
   const double xmin = -10;
   const double xmax =  10;
   const double dx = (xmax-xmin)/(N-1);
-  double dt = ;
+  double dt = 0.5*dx/V;
   const int Na = 10; // Number of output files up to tEnd
   const int Nk = int(tEnd/Na/dt);
 
   double* u0 = new double[N];
   double* u1 = new double[N];
-  double* h;
+  double* h;// wird benutzt, um die ZEIGER der beiden Pointer u0 und u1 zu vertauschen, statt die Eintraege in den Vektoren zu vertauschen, was viel Aufwand waere!
 
   stringstream strm;
 
@@ -39,10 +42,19 @@ int main(){
    for(int j=0; j<Nk; j++){
 
       // Put call to step function here
-
+     upwind(u1, u0, dt, dx, N, V);
+     //forward(u1, u0, dt, dx, N, V);
+     
       // swap arrays u0 <-> u1,
       // however do not copy values, be more clever ;)
-   }
+     
+     h = u0;
+     
+     u0 = u1;
+     
+     u1 = h;  
+     
+  }
    strm.str("");
    strm << "u_" << i;
    writeToFile(u0, strm.str(), dx, xmin, N);
@@ -55,6 +67,21 @@ int main(){
 }
 //-----------------------------------------------
 
+void forward(double* u1, double* u0, double dt, const double dx, const int N, const double V){
+ u1[0] = - V*dt*(u0[1])/(2*dx) + u0[0];
+  
+ for(int i=1; i<N; i++){
+ u1[i] = - V*dt*(u0[i+1]-u0[i-1])/(2*dx) + u0[i]; 
+ } 
+}
+
+void upwind(double* u1, double* u0, double dt, const double dx, const int N, const double V){
+ u1[0] = - V *dt*(u0[0]/dx) + u0[0];
+ 
+ for(int i=1; i<N; i++){
+ u1[i] = - V *dt*((u0[i]-u0[i-1])/dx) + u0[i]; 
+ } 
+}
 //-----------------------------------------------
 void initialize(double* const u, const double dx, const double xmin,
                 const int N)
